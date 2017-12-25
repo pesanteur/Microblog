330d1be4a78a0b0ed3f14ee5201ad1104a422c2e
Pesanteur <pesanteur123@gmail.com>
2017-12-24 20:14:16 -0500

    Additional changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,6 +8,10 @@ from flask_login import LoginManager
 from flask_mail import Mail
 import os
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +21,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -48,4 +54,8 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-from app import routes, models, errors
+@babel.localeselocator
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+from app import routes, models # <-- removed erros from this import!
